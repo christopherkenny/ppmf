73dfff1a1865f6f64a61a220bb9e2d1f805c5bad
Christopher Kenny <ctk44@cornell.edu>
2023-04-21 12:33:37 -0400

    add NULL option (undocumented purposely)

--- R/get_data.R
+++ R/get_data.R
@@ -22,18 +22,19 @@
 read_ppmf <- function(state, path){
   states <- ppmf::states
 
-  if(is.numeric(state)){
-    state <- stringr::str_pad(state, 2, 'left', '0')
-  }
-  which_state <- which(state == states$state)
-  if(length(which_state) == 0){
-    which_state <- which(state == states$state_code)
-  }
-  if(length(which_state) == 0){
-    stop('state does not match a state abbreviation or state fips.\n
+  if (!is.null(state)) {
+    if(is.numeric(state)){
+      state <- stringr::str_pad(state, 2, 'left', '0')
+    }
+    which_state <- which(state == states$state)
+    if(length(which_state) == 0){
+      which_state <- which(state == states$state_code)
+    }
+    if(length(which_state) == 0){
+      stop('state does not match a state abbreviation or state fips.\n
          Check `data(states)` columns state or state_code.')
+    }
   }
-
 
   if(!file.exists(path)){
     stop('`path` does not point to a file.')
@@ -42,9 +43,12 @@ read_ppmf <- function(state, path){
   cols_nom <- readr::read_lines(file = path, n_max = 1) |>
     stringr::str_split(pattern = ',', simplify = TRUE)
 
+  skip_s <- ifelse(is.null(state), 1, states$skip[which_state])
+  n_max_s <- ifelse(is.null(state), 312471327, states$n_max[which_state])
+
   readr::read_csv(path,
-           skip = states$skip[which_state],
-           n_max = states$n_max[which_state],
+           skip = skip_s,
+           n_max = n_max_s,
            col_types = readr::cols(.default = readr::col_character()),
            col_names = cols_nom)
 
